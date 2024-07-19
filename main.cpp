b21c1328705d3b1e12cd75c1e7c45afdd24a4983
deservate <mrclonetrooper2@gmail.com>
2024-07-19 14:12:29 +0200

    incorporated parent tree of country, province, pop, culture

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <vector>
 
+class Culture {
+    public:
+        std::string name;
+};
+
+class Religion {
+    public:
+        std::string name;
+};
+
+class Pop {
+    public:
+        Culture culture;
+};
+
 class Province {
     public:
         std::string name;
+        std::vector<Pop> pops;
 };
 
 class Country {
@@ -12,29 +28,72 @@ class Country {
         std::vector<Province> provinces;
 };
 
-Country CreateCountry(std::string name, std::vector<std::string> provinceNames) {
-    Country newCountry;
-    newCountry.name = name;
+std::vector<Culture> CreateCulturesList() {
 
-    std::vector<Province> newProvinceList;
-    
-    for (int i = 0; i < provinceNames.size(); i++) {
+    auto CreateCulture = [](std::string name) {
+        Culture newCulture;
+        newCulture.name = name;
+        return newCulture;
+    };
+
+    std::vector<Culture> cultures;
+    cultures.push_back(CreateCulture("Elmworthian"));
+    cultures.push_back(CreateCulture("Tashkavian"));
+
+    return cultures;
+}
+
+std::vector<Country> CreateCountriesList(std::vector<Culture> * ptrCultures) { 
+
+    auto CreatePop = [](Culture culture) {
+        Pop newPop;
+        newPop.culture = culture;
+        return newPop;
+    };
+
+    auto CreateProvince = [](std::string name, std::vector<Pop> pops) {
         Province newProvince;
-        newProvince.name = provinceNames[i];
-        newProvinceList.push_back(newProvince);
-    }
-    newCountry.provinces = newProvinceList;
+        newProvince.name = name;
+        newProvince.pops = pops;
+        return newProvince;
+    };
 
-    return newCountry;
+    auto CreateCountry = [](std::string name, std::vector<Province> provinces) {
+        Country newCountry;
+        newCountry.name = name;
+        newCountry.provinces = provinces;
+        return newCountry;
+    };
+
+    std::vector<Country> countries;
+
+    // ==== Elmworth ====
+    std::vector<Province> provinces = {
+        CreateProvince("Westfeld", {CreatePop(ptrCultures->at(0)), CreatePop(ptrCultures->at(1))})
+        ,CreateProvince("Riverton", {CreatePop(ptrCultures->at(0)), CreatePop(ptrCultures->at(1))})
+        ,CreateProvince("Hawkshire", {CreatePop(ptrCultures->at(0)), CreatePop(ptrCultures->at(1))})
+    };
+
+    countries.push_back(CreateCountry("Elmworth", provinces));
+
+    return countries;
 }
 
 int main() {
+    
+    // create the game world
+    std::vector<Culture> cultures = CreateCulturesList();
 
-    std::vector<std::string> provinceNames = {"Aberworth", "Bennedit", "Comberwhile"};
 
-    Country country1 = CreateCountry("Countristan", provinceNames);
-    std::cout << "Hello World!";
-    std::cout << "Hello World!" << country1.provinces[0].name << std::endl;
+    std::vector<Country> countries = CreateCountriesList(&cultures);
+
+    std::cout << countries[0].provinces[0].pops[0].culture.name << std::endl;
+    std::cout << cultures[0].name << std::endl;
+
+    cultures[0].name = "NEWNAME";
+
+    std::cout << countries[0].provinces[0].pops[0].culture.name << std::endl;
+    std::cout << cultures[0].name << std::endl;
 
     return 0;
 }
